e1f89acc3877e1b9b07cb3f81835f5aa92118f59
Ben Gott <bengott@gmail.com>
2014-07-22 14:03:37 -0700

    Testing fetch and transform options on allow/deny rules

--- foo.js
+++ foo.js
@@ -1,19 +1,45 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to foo.";
-  };
+// Test app to try out fetch and transform options on allow/deny rules
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+Foos = new Meteor.Collection("foos", {
+  transform: function (doc) { return _.extend(doc, {field4: "bar"}); }
+});
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Foos.find().count() === 0) {
+      Foos.insert({name: "bap", field1: "foo", field2: "foo", field3: "foo", field4: "foo"});
+      Foos.insert({name: "bep", field1: "foo", field2: "foo", field3: "foo", field4: "foo"});
+      Foos.insert({name: "bip", field1: "foo", field2: "foo", field3: "foo", field4: "foo"});
+      Foos.insert({name: "bop", field1: "foo", field2: "foo", field3: "foo", field4: "foo"});
+      Foos.insert({name: "bup", field1: "foo", field2: "foo", field3: "foo", field4: "foo"});
+      Foos.insert({name: "byp", field1: "foo", field2: "foo", field3: "foo", field4: "foo"});
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Foos.allow({
+  insert: function (userId, doc) {
+    console.log(doc);
+    return doc.name !== "";
+  },
+  update: function (userId, doc, fields, modifier) {
+    console.log(doc);
+    return doc.field4 === "foo";
+  },
+  remove: function (userId, doc) {
+    console.log(doc);
+    return doc.field4 === "foo";
+  },
+  // Comment/uncomment these options to test what they do
+  fetch: ["field4"],
+  transform: null
+});
+
+Foos.deny({
+});
+
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to foo.";
+  };
 }
