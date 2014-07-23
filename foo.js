2309f66c8ec659a597bf2b8bb47b6304ca98ebd5
Ben Gott <bengott@gmail.com>
2014-07-22 19:15:33 -0700

    Add deny rules and more comments

--- foo.js
+++ foo.js
@@ -1,9 +1,11 @@
 // Test app to try out fetch and transform options on allow/deny rules
 
+// Collection with a transform on it
 Foos = new Meteor.Collection("foos", {
   transform: function (doc) { return _.extend(doc, {field4: "bar"}); }
 });
 
+// Sample data inserted on first startup
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Foos.find().count() === 0) {
@@ -17,6 +19,7 @@ if (Meteor.isServer) {
   });
 }
 
+// Allow/Deny Rules - doc logged to console
 Foos.allow({
   insert: function (userId, doc) {
     console.log(doc);
@@ -30,14 +33,30 @@ Foos.allow({
     console.log(doc);
     return doc.field4 === "foo";
   },
-  // Comment/uncomment these options to test what they do
-  fetch: ["field4"],
-  transform: null
+  // Comment/uncomment these options and then try update/remove from browser console
+  fetch: ["field4"], // also try fetching a different field
+  transform: null // or a different transform
 });
 
 Foos.deny({
+  insert: function (userId, doc) {
+    console.log(doc);
+    return doc.name === "foo";
+  },
+  update: function (userId, doc, fields, modifier) {
+    console.log(doc);
+    return doc.field4 === "bar";
+  },
+  remove: function (userId, doc) {
+    console.log(doc);
+    return doc.field4 === "bar";
+  },
+  // Comment/uncomment these options and then try update/remove from browser console
+  fetch: ["field4"], // also try fetching a different field
+  transform: null // or a different transform
 });
 
+// Template Helpers
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to foo.";
